d03292acc6dd46074217f882c36ca1cb0b288bef
yatinjaie <yatin.jaie@gmail.com>
2021-11-18 21:37:42 +0530

    Create 9-subsetsum.java

---
+++ 9-subsetsum.java
@@ -0,0 +1,8 @@
+/* This problem is a variation of leetcode-416 - Subset problem.
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that difference is equal to given difference.
+nums = [3,2,1,4]
+diff = 4
+
+s2 = (sum of arra - diff)/2;
+Solve same way like leetcode 416, but change sum.
+*/
