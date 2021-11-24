3d3ae659f01badbc55ebb2c4e7b16e876152b49d
yatinjaie <yatin.jaie@gmail.com>
2021-11-24 22:24:01 +0530

    Create 15-lc-123-buy-sell-stock-2.java
    tle

---
+++ 15-lc-123-buy-sell-stock-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        Map<String,Integer> map = new HashMap<>();
+        return helper(prices,0,true,2,map);
+    }
+    
+    public int helper(int[] prices, int curr, boolean canBuy,int transactionCount, Map<String,Integer> map){
+        if(curr>=prices.length){
+            return 0;
+        }
+        
+        if(transactionCount<=0){
+            return 0;
+        }
+        String currentKey = curr + String.valueOf(canBuy) + transactionCount;
+        if(map.containsKey(currentKey)){
+            return map.get(currentKey);
+        }
+        
+        int idle = helper(prices,curr+1,canBuy,transactionCount,map);
+        int buy =0,sell=0;
+        if(canBuy){
+            buy = -prices[curr] + helper(prices,curr+1,!canBuy,transactionCount,map);
+        }
+        if(!canBuy){
+         sell = prices[curr] + helper(prices,curr+1,!canBuy,transactionCount-1,map);
+        }
+        map.put(currentKey, Math.max(Math.max(idle,buy),sell));
+        return map.get(currentKey);
+    }
+}
+
+// TC :  O(n)*O(2)*O(1) = O(2*n) = O(n)
+// SC : O(n)
